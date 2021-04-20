ef11f7a866dfed75910a534e2a6cacd99d8e2c38
Rowan-Paul <rpflynn80@gmail.com>
2021-04-20 14:21:22 +0200

    Add insert data for pv & grid

--- backend/seed.py
+++ backend/seed.py
@@ -3,25 +3,78 @@ con = sqlite3.connect('database.db')
 
 cur = con.cursor()
 
-con.execute("DROP TABLE users")
-con.execute("DROP TABLE weather")
+con.execute("DROP TABLE IF EXISTS users")
+con.execute("DROP TABLE IF EXISTS weather")
+con.execute("DROP TABLE IF EXISTS battery")
+con.execute("DROP TABLE IF EXISTS grid")
+con.execute("DROP TABLE IF EXISTS pv")
 
 # Create tables
 cur.execute('''CREATE TABLE IF NOT EXISTS users
-               (signupDate  text    NOT NULL, 
-               name         text    NOT NULL, 
-               password     text    NOT NULL, 
-               isAdmin      integer)''')
+               (signupDate  TEXT    NOT NULL, 
+               name         TEXT    NOT NULL, 
+               password     TEXT    NOT NULL, 
+               isAdmin      INTEGER)''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS weather
-               (date        text     NOT NULL,
-               temperature  real     NOT NULL, 
-               cloud        integer  NOT NULL, 
-               wind         real     NOT NULL, 
-               pressure     integer  NOT NULL)''')
+               (date        TEXT     NOT NULL,
+               temperature  REAL     NOT NULL, 
+               cloud        INTEGER  NOT NULL, 
+               wind         REAL     NOT NULL, 
+               pressure     INTEGER  NOT NULL)''')
+
+# (tables from the old database)
+cur.execute('''CREATE TABLE IF NOT EXISTS grid (
+    [No] INTEGER            PRIMARY KEY AUTOINCREMENT,
+    Time [UNISGNED BIG INT],
+    V1   REAL,
+    V2   REAL,
+    V3   REAL,
+    I1   REAL,
+    I2   REAL,
+    I3   REAL,
+    P1   REAL,
+    P2   REAL,
+    P3   REAL,
+    Q1   REAL,
+    Q2   REAL,
+    Q3   REAL,
+    S1   REAL,
+    S2   REAL,
+    S3   REAL,
+    PF1  REAL,
+    PF2  REAL,
+    PF3  REAL,
+    F    REAL
+)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS pv (
+    [No] INTEGER            PRIMARY KEY AUTOINCREMENT,
+    Time [UNISGNED BIG INT],
+    V1   REAL,
+    V2   REAL,
+    V3   REAL,
+    I1   REAL,
+    I2   REAL,
+    I3   REAL,
+    P1   REAL,
+    P2   REAL,
+    P3   REAL,
+    Q1   REAL,
+    Q2   REAL,
+    Q3   REAL,
+    S1   REAL,
+    S2   REAL,
+    S3   REAL,
+    PF1  REAL,
+    PF2  REAL,
+    PF3  REAL,
+    F    REAL
+)''')
 
 # Insert data
-cur.execute("INSERT INTO users VALUES (datetime('now'), 'rowan','password',1)")
+cur.execute(
+    "INSERT INTO users VALUES (datetime('now'), 'rowan','password',1)")
 
 cur.execute(
     "INSERT INTO weather VALUES ('2021-04-20 09:45:14', 10.38, 36, 4.73, 1023)")
@@ -72,19 +125,92 @@ cur.execute(
 cur.execute(
     "INSERT INTO weather VALUES ('2021-04-21 8:45:14', 13.46, 73, 4.08, 1021)")
 
+cur.execute(
+    '''INSERT INTO grid VALUES(1, 1617589974, 234.6, 234.2, 234.3, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, -0.017, -0.008, -0.026, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(2, 1617590077, 234.5, 234.1, 234.4, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, -0.042, 0.028, -0.031, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(3, 1617590190, 234.5, 234.0, 234.4, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, -0.001, 0.039, -0.036, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(4, 1617590275, 234.6, 234.0, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.007, 0.015, -0.033, 50.03)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(5, 1617590375, 234.6, 234.1, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.007, 0.015, -0.033, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(6, 1617590465, 234.6, 234.1, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.007, 0.015, -0.033, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(7, 1617590558, 234.7, 234.1, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, -0.029, 0.012, -0.045, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(8, 1617590661, 234.6, 234.1, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.007, 0.027, -0.03, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(9, 1617590964, 234.6, 234.1, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, -0.009, 0.03, -0.04, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(10, 1617591036, 234.0, 233.8, 234.0, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.031, 0.037, -0.031, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(11, 1617591370, 234.5, 234.0, 234.2, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.009, 0.017, -0.031, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(12, 1617591450, 234.6, 234.2, 234.5, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, -0.003, 0.027, -0.036, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(13, 1617591613, 234.8, 234.5, 234.6, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.001, 0.003, -0.027, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(14, 1617591700, 234.4, 234.1, 234.3, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, -0.005, 0.024, -0.04, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(15, 1617591784, 234.2, 234.0, 234.1, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, -0.005, 0.024, -0.04, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(16, 1617591925, 234.1, 233.7, 234.0, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.009, 0.016, -0.029, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(17, 1617592002, 233.9, 233.5, 233.7, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.004, 0.003, -0.052, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(18, 1617592081, 234.1, 233.6, 233.9, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.004, 0.003, -0.052, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(19, 1617592162, 234.1, 233.7, 234.0, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 180.0, 180.0, 0.004, 0.003, -0.052, 50.0)''')
+cur.execute(
+    '''INSERT INTO grid VALUES(20, 1617592289, 234.0, 233.6, 233.9, 0.81, 0.75, 0.84, 0.0, 0.0, 0.0, 180.0, 150.0, 180.0, 180.0, 150.0, 180.0, 0.021, 0.024, -0.039, 50.0)''')
+
+cur.execute(
+    '''INSERT INTO pv  VALUES (1, 1617589961, 234.5, 234.2, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (2, 1617590041, 234.3, 233.9, 234.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (3, 1617590152, 234.5, 234.1, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (4, 1617590248, 234.6, 234.1, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.03)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (5, 1617590361, 234.5, 234.1, 234.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (6, 1617590512, 234.7, 234.2, 234.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (7, 1617590625, 234.5, 234.1, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (8, 1617590739, 234.7, 234.2, 234.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (9, 1617590788, 234.5, 234.2, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (10, 1617590917, 234.2, 234.0, 234.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (11, 1617591008, 234.1, 233.8, 234.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.03)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (12, 1617591103, 234.3, 233.9, 234.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (13, 1617591250, 234.3, 233.9, 234.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (14, 1617591587, 234.8, 234.5, 234.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (15, 1617591682, 234.6, 234.2, 234.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (16, 1617591766, 234.2, 234.2, 234.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (17, 1617591908, 234.2, 233.8, 234.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (18, 1617592126, 234.0, 233.7, 234.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (19, 1617592251, 233.9, 233.6, 234.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+cur.execute(
+    '''INSERT INTO pv  VALUES (20, 1617592536, 234.0, 233.7, 234.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 50.0)''')
+
 # Save (commit) the changes
 con.commit()
 
-# Print the inserted data
-print("Users:")
-for row in cur.execute('SELECT * FROM users'):
-    print(row)
-
-print("")
-
-print("Weather:")
-for row in cur.execute('SELECT * FROM weather'):
-    print(row)
-
 # Close the connection
 con.close()
+
+print("Finished")
