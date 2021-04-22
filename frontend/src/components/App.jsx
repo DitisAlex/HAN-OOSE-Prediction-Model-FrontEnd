c3632528fe67a8dd56f9fa82b7ae7e1ac37c932b
DitisAlex <alexcheng0314@gmail.com>
2021-04-22 12:03:12 +0200

    Added reactstrap & updated boilerplate code (navbar)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
+
 import Login from './Login';
 import Logout from './Logout';
 import AdminPage from './AdminPage';
@@ -17,24 +32,47 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand">Prediction Model</a>
-                    <div class="collapse navbar-collapse justify-content-between align-items-center w-100" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <Link to="/" class="nav-link">DataA</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/data" class="nav-link">DataB</Link>
-                            </li>
-                        </ul>
-                        <ul class="navbar-nav flex-row justify-content-md-center justify-content-start flex-nowrap">
-                            <li class="nav-item">
-                                <Link to="/login" class="nav-link">Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+                <Navbar color="dark" dark expand="md">
+                    <NavbarBrand>Prediction Model</NavbarBrand>
+                    <Nav className="container-fluid" navbar>
+                        <NavItem>
+                            <Link to="/">
+                                <NavLink>DataA</NavLink>
+                            </Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/data">
+                                <NavLink>DataB</NavLink>
+                            </Link>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Models
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <Link to="/">
+                                    <DropdownItem>
+                                    DataA
+                                    </DropdownItem>
+                                </Link>
+                                <Link to="/data">
+                                    <DropdownItem>
+                                    DataB
+                                    </DropdownItem>
+                                </Link>
+                                <DropdownItem divider />
+                                    <DropdownItem>
+                                        Exit
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        <NavItem className="ml-auto">
+                        <Link to="/login">
+                                <NavLink>Login</NavLink>
+                            </Link>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
                 <div className="container">
                     <Switch>
                         <Route exact path="/" component={DataPageA} />
