ebb9413a393869ba8fc7d2ae0f99c508bd5f569a
ilivss <ilivss@pm.mex>
2021-04-24 08:40:42 +0200

    onderzoek sqlite

---
+++ docs/onderzoek-sqlite/db/seed/seedAll.py
@@ -0,0 +1,10 @@
+from seed import seed
+from seedTest import seedTest
+from seedTables import seedTables
+from seedTablesTest import seedTablesTest
+
+# uncomment a function to skip it
+seed()
+# seedTables()
+seedTest()
+# seedTablesTest()
