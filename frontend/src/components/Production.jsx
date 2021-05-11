e8697fc0bc98b77728b6eb447d8972cb50c88183
Bart de Kinkelaar <bar.dekinkelaar@student.han.nl>
2021-05-11 11:12:52 +0200

    Voor nu basis gelegd voor productiepage

--- frontend/src/components/Production.jsx
+++ frontend/src/components/Production.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import '../css/style.css';
 export default function Production(props) {
 
     return (
-        <div class="container">
-            <div class="filter">
-                <h2>Filter</h2>
-                <form>
-                    <label>
-                        Aantal uren
-                        <input type="text" />
-                    </label>
-                    <label>
-                        Tijdstip
-                        <input type="text" />
-                    </label>
-                    <label>
-                        Voorspelling
-                        <input type="checkbox" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-            <div class="energyContainer">
-                <h1>Energy production</h1>
-                <p>Welcome</p>
+        <div className="production-page">
+            <div class="row">
+                <div className="col-3 filter">
+                    <h2>Filter</h2>
+                    <Form className="productionForm">
+                        <FormGroup>
+                            <Label for="hours">Hours</Label>
+                            <Input type="email" name="email" id="hours" placeholder="Hours to show" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="time">Time</Label>
+                            <Input type="text" name="time" id="time" placeholder="Time"/>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                            <Input type="checkbox" id="prediction" />{' '}
+                            Prediction
+                            </Label>
+                        </FormGroup>
+                        <Button>Submit</Button>
+                    </Form>
+                </div>
+                <div className="col-9 energyContainer">
+                    <h1>Energy production</h1>
+                    <p>Welcome</p>
+                </div>
             </div>
         </div>
     )
