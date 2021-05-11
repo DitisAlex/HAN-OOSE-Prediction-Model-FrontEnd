db0aa2a7004fe27b54e001069abf3beb73d7cea6
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-11 10:05:11 +0200

    Fix import

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 
-import { graphsReducer } from './cards/reducer'
+import { graphsReducer } from './graphs/reducer'
 
 const rootReducer = combineReducers({
   graphs: graphsReducer,
