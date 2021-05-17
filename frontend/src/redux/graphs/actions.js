83bcf4c551a443273d082b35b49f04eaf46ca28c
DitisAlex <alexcheng0314@gmail.com>
2021-05-17 14:50:40 +0200

    Vanuit een API call (met dummy data) kan er via Redux een grafiek gegenereert worden

--- frontend/src/redux/graphs/actions.js
+++ frontend/src/redux/graphs/actions.js
@@ -1,5 +1,10 @@
 import * as types from './types'
 
+let test_data = {
+    labels: ["10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"],
+    values: [8, 10, 15, 13, 17, 18, 22, 19]
+}
+
 export const fetchConsumption = () => (dispatch) => {
     let url = '/';
 
@@ -16,7 +21,7 @@ export const fetchConsumption = () => (dispatch) => {
         console.log(response)
         dispatch({
             type: types.FETCHED_CONSUMPTION,
-            payload: "test",
+            payload: test_data,
         })
     }).catch((err) => {
         console.log('Failed to fetch consumption data', err)
--- frontend/src/redux/graphs/reducer.js
+++ frontend/src/redux/graphs/reducer.js
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.FETCHED_CONSUMPTION:
-      return {...state, text: action.payload}
+      return {...state, consumption: action.payload}
     default:
       return state
   }
