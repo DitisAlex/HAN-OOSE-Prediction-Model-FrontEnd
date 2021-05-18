6b02098b58ab11a31992a36b5e757f2b8e12f532
DitisAlex <alexcheng0314@gmail.com>
2021-05-18 13:18:41 +0200

    Removed unused test_data from actions.js + Moved README to /docs

--- frontend/src/redux/graphs/actions.js
+++ frontend/src/redux/graphs/actions.js
@@ -1,10 +1,5 @@
 import * as types from './types'
 
-let test_data = {
-    labels: ["10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM"],
-    values: [8, 10, 15, 13, 17, 18, 22, 19]
-}
-
 export const fetchConsumption = () => {
     let url = '/consumption'
 
