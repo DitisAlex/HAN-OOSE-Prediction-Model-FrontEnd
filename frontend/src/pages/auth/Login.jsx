439cca93aadf21c9adb049ea26ce592ceb888e04
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-18 09:20:26 +0200

    Fix usage of class instead of className
    class doesn't exist in React

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Login(props){
-
-    return (
-        <div className="container">
-            <h1>Login Page</h1>
-            <Link to="/admin">
-                <button type="button" class="btn btn-secondary">Login</button>
-            </Link>
-        </div>
-    )
+export default function Login(props) {
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+      <Link to="/admin">
+        <button type="button" className="btn btn-secondary">
+          Login
+        </button>
+      </Link>
+    </div>
+  )
 }
