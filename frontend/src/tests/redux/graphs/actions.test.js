7efae7421463eb5d8d9268d95fe7ab86ecf85fce
DitisAlex <alexcheng0314@gmail.com>
2021-06-03 11:51:39 +0200

    Redux action & reducer test

--- frontend/src/tests/redux/graphs/actions.test.js
+++ frontend/src/tests/redux/graphs/actions.test.js
@@ -90,4 +90,43 @@ describe('Graphs actions', () => {
       })
     })
   })
+
+  it('Dispatches FETCHED_PREDICTION after fetching prediction data', () => {
+    expect.assertions(2)
+    const expectedResponse = [
+      { datetime: '2021-05-31 10:30', labels: '10:30 AM', value: 6 },
+      { datetime: '2021-05-31 11:30', labels: '11:30 AM', value: 5 },
+      { datetime: '2021-05-31 12:30', labels: '12:30 PM', value: 6 },
+      { datetime: '2021-05-31 13:30', labels: '1:30 PM', value: 7 },
+    ]
+    const mResponse = {
+      json: jest.fn().mockResolvedValueOnce(expectedResponse),
+    }
+    global.fetch = jest.fn().mockResolvedValueOnce(mResponse)
+
+    const expectedActions = [
+      {
+        type: types.FETCHED_PREDICTION,
+        payload: {
+          labels: ['10:30 AM', '11:30 AM', '12:30 PM', '1:30 PM'],
+          values: [6, 5, 6, 7],
+          datetime: [
+            '2021-05-31 10:30',
+            '2021-05-31 11:30',
+            '2021-05-31 12:30',
+            '2021-05-31 13:30',
+          ],
+        },
+      },
+    ]
+
+    return store.dispatch(actionTypes.fetchPrediction(4)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+      expect(global.fetch).toBeCalledWith('/prediction/4', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    })
+  })
 })
