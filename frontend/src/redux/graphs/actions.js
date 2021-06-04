0843c0cc6b783461ced607dc6ea0362f8760456c
Rowan-Paul <contact@rowanpaulflynn.com>
2021-06-04 10:56:59 +0200

    Àdd fetching consumption

--- frontend/src/redux/graphs/actions.js
+++ frontend/src/redux/graphs/actions.js
@@ -3,8 +3,8 @@ import * as types from './types'
 export const fetchConsumption = () => {
   let url = '/energy/consumption'
 
-  return (dispatch) => {
-    return fetch(url, {
+  return (dispatch) =>
+    fetch(url, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -14,17 +14,21 @@ export const fetchConsumption = () => {
         var tempObj = {
           labels: [],
           values: [],
+          datetime: [],
         }
         res.forEach((element) => {
           tempObj.labels.push(element.labels)
           tempObj.values.push(element.values)
+          tempObj.datetime.push(element.datetime)
         })
         return dispatch({
           type: types.FETCHED_CONSUMPTION,
           payload: tempObj,
         })
       })
-  }
+      .catch((err) => {
+        console.log('Failed to fetch consumption data', err)
+      })
 }
 
 export const fetchProduction = () => {
@@ -59,7 +63,7 @@ export const fetchProduction = () => {
 }
 
 export const fetchPrediction = (hours) => {
-  let url = "/prediction/" + hours;
+  let url = '/prediction/' + hours
 
   return (dispatch) =>
     fetch(url, {
@@ -88,4 +92,45 @@ export const fetchPrediction = (hours) => {
       .catch((err) => {
         console.log('Failed to fetch prediction data', err)
       })
+}
+
+export const setHours = (hours, type) => {
+  return (dispatch, getState) => {
+    let data = ''
+    if (type === 'production') {
+      data = getState().graphs.production
+    } else if (type === 'consumption') {
+      data = getState().graphs.consumption
+    }
+
+    let earliestDate = new Date()
+    earliestDate.setHours(earliestDate.getHours() - 4)
+
+    let newObject = {
+      labels: [],
+      values: [],
+      datetime: [],
+    }
+
+    for (let i = 0; i < data.labels?.length; i++) {
+      const dataPointDate = new Date(data.datetime[i])
+
+      if (earliestDate < dataPointDate) {
+        newObject.labels.push(data.labels[i])
+        newObject.values.push(data.values[i])
+        newObject.datetime.push(data.datetime[i])
+      }
+    }
+
+    if (newObject.labels.length === 1) {
+      newObject.labels.push(newObject.labels[0])
+      newObject.values.push(newObject.values[0])
+      newObject.datetime.push(newObject.values[0])
+    }
+
+    return dispatch({
+      type: types.SET_HOURS,
+      payload: { type: type, selected: newObject },
+    })
+  }
 }
