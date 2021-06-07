ac1f2818bea9281647022eb47cd3b1a0e7c0c766
Rowan-Paul <contact@rowanpaulflynn.com>
2021-06-07 13:07:45 +0200

    Fix SQ issues
    code quality

--- frontend/src/tests/puppeteer/main.e2e.js
+++ frontend/src/tests/puppeteer/main.e2e.js
@@ -1,64 +1,65 @@
-const puppeteer = require("puppeteer");
-jest.setTimeout(250000);
+const puppeteer = require('puppeteer')
+jest.setTimeout(250000)
 
-describe("Prediction Model E2E Tests", () => {
-    const testSpeed = 10;
-    let browser;
-    let page;
+describe('Prediction Model E2E Tests', () => {
+  const customSelect = 'select[name="customSelect"]'
+  const testSpeed = 10
+  let browser
+  let page
 
-    beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: testSpeed,
-            defaultViewport: null,
-            args: [`--window-size=1920,1080`],
-        });
-        page = await browser.newPage();
-
-        await page.goto("http://localhost:3000/")
-        await page.waitForTimeout(1500)
-    });
-
-    afterAll(async () => {
-        await page.waitForTimeout(5000)
-        await browser.close();
-    });
-
-    test("Navigate to /production page", async () => {
-        const hours = ["1", "2", "3", "4"]
-
-        await page.click('button[id="Solar power"]')
-        await page.waitForTimeout(1500)
-        for (i = 0; i < hours.length; i++) {
-            await page.click('select[name="customSelect"]')
-            await page.waitForTimeout(1500)
-            await page.select('select#exampleCustomSelect', hours[i])
-            await page.click('select[name="customSelect"]')
-            await page.waitForTimeout(2000)
-        }
-        await page.waitForTimeout(1500)
-    });
-
-    test("Test /production prediction data", async () => {
-        const hours = ["1", "2", "3", "4"]
-
-        await page.click('input[name="customSwitch"]')
-        await page.waitForTimeout(1500)
-        for (i = 0; i < hours.length; i++) {
-            await page.click('select[name="customSelect"]')
-            await page.waitForTimeout(1500)
-            await page.select('select#exampleCustomSelect', hours[i])
-            await page.click('select[name="customSelect"]')
-            await page.waitForTimeout(2000)
-        }
-        await page.click('input[name="customSwitch"]')
-        await page.waitForTimeout(1500)
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: testSpeed,
+      defaultViewport: null,
+      args: [`--window-size=1920,1080`],
     })
+    page = await browser.newPage()
 
-    test("Navigate to /consumption page", async () => {
-        await page.click('a[id="dropdownToggle"')
-        await page.waitForTimeout(1500)
-        await page.click('a[id="consumptionPage"]')
-        await page.waitForTimeout(1500)
-    });
-});
+    await page.goto('http://localhost:3000/')
+    await page.waitForTimeout(1500)
+  })
+
+  afterAll(async () => {
+    await page.waitForTimeout(5000)
+    await browser.close()
+  })
+
+  test('Navigate to /production page', async () => {
+    const hours = ['1', '2', '3', '4']
+
+    await page.click('button[id="Solar power"]')
+    await page.waitForTimeout(1500)
+    for (let i = 0; i < hours.length; i++) {
+      await page.click(customSelect)
+      await page.waitForTimeout(1500)
+      await page.select('select#exampleCustomSelect', hours[i])
+      await page.click(customSelect)
+      await page.waitForTimeout(2000)
+    }
+    await page.waitForTimeout(1500)
+  })
+
+  test('Test /production prediction data', async () => {
+    const hours = ['1', '2', '3', '4']
+
+    await page.click('input[name="customSwitch"]')
+    await page.waitForTimeout(1500)
+    for (i = 0; i < hours.length; i++) {
+      await page.click(customSelect)
+      await page.waitForTimeout(1500)
+      await page.select('select#exampleCustomSelect', hours[i])
+      await page.click(customSelect)
+      await page.waitForTimeout(2000)
+    }
+    await page.click('input[name="customSwitch"]')
+    await page.waitForTimeout(1500)
+  })
+
+  test('Navigate to /consumption page', async () => {
+    await page.click('a[id="dropdownToggle"')
+    await page.waitForTimeout(1500)
+    await page.click('a[id="consumptionPage"]')
+    await page.waitForTimeout(1500)
+  })
+})
